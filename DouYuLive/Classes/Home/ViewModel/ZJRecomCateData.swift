104de236a8fec7c85349e6203e091505b2629f58
dengzj <dengzj@canplay.com.cn>
2018-08-28 16:26:47 +0800

    修改网络请求

---
+++ DouYuLive/Classes/Home/ViewModel/ZJRecomCateData.swift
@@ -0,0 +1,13 @@
+//
+//  ZJRecomCateData.swift
+//  DouYuLive
+//
+//  Created by 邓志坚 on 2018/8/28.
+//  Copyright © 2018年 邓志坚. All rights reserved.
+//
+
+import UIKit
+
+class ZJRecomCateData: NSObject {
+
+}
