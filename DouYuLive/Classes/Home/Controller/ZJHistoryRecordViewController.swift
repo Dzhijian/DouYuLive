30d008208d978e84c62c19bdb94d1017600fe2b9
dengzj <dengzj@canplay.com.cn>
2018-09-02 02:00:06 +0800

    添加自定义轮播图（一）

---
+++ DouYuLive/Classes/Home/Controller/ZJHistoryRecordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ZJHistoryRecordViewController.swift
+//  DouYuLive
+//
+//  Created by 邓志坚 on 2018/9/2.
+//  Copyright © 2018年 邓志坚. All rights reserved.
+//
+
+import UIKit
+
+class ZJHistoryRecordViewController: ZJBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DouYuLive/Classes/Tools/ZJCarouselView/ZJCarouselCell.swift
@@ -0,0 +1,13 @@
+//
+//  ZJCarouselCell.swift
+//  DouYuLive
+//
+//  Created by 邓志坚 on 2018/9/2.
+//  Copyright © 2018年 邓志坚. All rights reserved.
+//
+
+import UIKit
+
+class ZJCarouselCell: UICollectionViewCell {
+    
+}
